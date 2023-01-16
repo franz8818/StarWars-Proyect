efe5519b30aac73cb05052fd908ec36e964dee5d
Franz Seidel <franz8818@gmail.com>
2023-01-16 01:44:40 +0000

    estetica 1.2

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-//import react into the bundle
+//import react into the bundle CONFIGURACION -> USAR BOOSTRAP O ESTILOS PROPIOS
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
--- src/js/layout.js
+++ src/js/layout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"; 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
@@ -18,6 +18,7 @@ const Layout = () => {
 
   return (
     <div>
+      {/* FUNCIONA COMO ROUTER, MANEJANDO VISTAS, SE ENCARGA DE HACER EL MATCH */}
       <BrowserRouter basename={basename}>
         <ScrollToTop>
           <Navbar />
